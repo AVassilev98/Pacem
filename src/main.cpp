12c1d20638f1e04e13a9c6f49445eb32df130c9a
Anton Vassilev <avvassil@uwaterloo.ca>
2022-11-13 02:34:04 -0500

    Add instance and validation layer

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,148 @@
+#include "array"
+#include "iostream"
+#include "vector"
+#include <cstring>
+#include <vulkan/vk_enum_string_helper.h>
+#include <vulkan/vulkan_core.h>
+
+#ifndef NDEBUG
+#define VK_LOG_ERR(f_)                                                                                                                         \
+    {                                                                                                                                          \
+        VkResult retCode = f_;                                                                                                                 \
+        if (retCode != VK_SUCCESS)                                                                                                             \
+        {                                                                                                                                      \
+            std::cerr << string_VkResult(retCode) << " Returned from " << __FUNCTION__ << "in " << __FILE__ << ":" << __LINE__ << std::endl;   \
+        }                                                                                                                                      \
+    }
+#else
+#define VK_LOG_ERR(f_) f_
+#endif
+
+static VkBool32 message(VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity, VkDebugUtilsMessageTypeFlagsEXT messageTypes,
+                        const VkDebugUtilsMessengerCallbackDataEXT *pCallbackData, void *pUserData)
+{
+    if (messageSeverity & VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT)
+    {
+        std::cerr << "[ERROR] ";
+    }
+    else if (messageSeverity & VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT)
+    {
+        std::cerr << "[WARNING] ";
+    }
+
+    if (messageTypes & VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT)
+    {
+        std::cerr << "General: ";
+    }
+    if (messageTypes & VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT)
+    {
+        std::cerr << "Validation: ";
+    }
+    if (messageTypes & VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT)
+    {
+        std::cerr << "Performance: ";
+    }
+
+    std::cerr << pCallbackData->pMessage << std::endl << std::endl;
+    return VK_FALSE;
+}
+
+static VkDebugUtilsMessengerCreateInfoEXT getMessengerCreateInfo()
+{
+    VkDebugUtilsMessengerCreateInfoEXT messengerCreateInfo = {VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT};
+    messengerCreateInfo.messageSeverity = VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT;
+    messengerCreateInfo.messageType = VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT |
+                                      VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT;
+    messengerCreateInfo.pfnUserCallback = message;
+
+    return messengerCreateInfo;
+}
+
+static VkDebugUtilsMessengerEXT createDebugMessenger(VkInstance instance)
+{
+    if (!instance)
+    {
+        return nullptr;
+    }
+
+    using MessengerFuncPtr = PFN_vkCreateDebugUtilsMessengerEXT;
+    MessengerFuncPtr createDebugMessagerFunc = (MessengerFuncPtr)vkGetInstanceProcAddr(instance, "vkCreateDebugUtilsMessengerEXT");
+    if (!createDebugMessagerFunc)
+    {
+        std::cerr << "Could not find proc addr vkCreateDebugUtilsMessengerEXT" << std::endl;
+        return nullptr;
+    }
+
+    VkDebugUtilsMessengerCreateInfoEXT messengerCreateInfo = getMessengerCreateInfo();
+    VkDebugUtilsMessengerEXT messenger = nullptr;
+    VK_LOG_ERR(createDebugMessagerFunc(instance, &messengerCreateInfo, nullptr, &messenger));
+    return messenger;
+}
+
+static void destroyDebugMessenger(VkInstance instance, VkDebugUtilsMessengerEXT messenger)
+{
+    if (!instance || !messenger)
+    {
+        return;
+    }
+
+    using MessengerFuncPtr = PFN_vkDestroyDebugUtilsMessengerEXT;
+    MessengerFuncPtr destroyDebugMessagerFunc = (MessengerFuncPtr)vkGetInstanceProcAddr(instance, "vkDestroyDebugUtilsMessengerEXT");
+    if (!destroyDebugMessagerFunc)
+    {
+        std::cerr << "Could not find proc addr vkDestroyDebugUtilsMessengerEXT" << std::endl;
+        return;
+    }
+
+    destroyDebugMessagerFunc(instance, messenger, nullptr);
+}
+
+static VkInstance createInstance()
+{
+    VkInstanceCreateInfo instanceCreateInfo = {VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO};
+
+    uint32_t layerCount = 0;
+    VK_LOG_ERR(vkEnumerateInstanceLayerProperties(&layerCount, nullptr));
+    std::vector<VkLayerProperties> availableLayers(layerCount);
+    VK_LOG_ERR(vkEnumerateInstanceLayerProperties(&layerCount, availableLayers.data()));
+
+    VkApplicationInfo appInfo = {VK_STRUCTURE_TYPE_APPLICATION_INFO};
+    appInfo.pApplicationName = "Para-ray";
+    appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+    appInfo.pEngineName = "No Engine";
+    appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+    appInfo.apiVersion = VK_API_VERSION_1_3;
+
+#ifndef NDEBUG
+    static const char *validationLayerName = "VK_LAYER_KHRONOS_validation";
+    static const char *debugExtension = VK_EXT_DEBUG_UTILS_EXTENSION_NAME;
+
+    VkDebugUtilsMessengerCreateInfoEXT messengerCreateInfo = getMessengerCreateInfo();
+    instanceCreateInfo.enabledExtensionCount = 1;
+    instanceCreateInfo.ppEnabledExtensionNames = &debugExtension;
+
+    instanceCreateInfo.enabledLayerCount = 1;
+    instanceCreateInfo.ppEnabledLayerNames = &validationLayerName;
+    instanceCreateInfo.pNext = &messengerCreateInfo;
+#endif
+
+    VkInstance instance = nullptr;
+    VK_LOG_ERR(vkCreateInstance(&instanceCreateInfo, nullptr, &instance));
+    return instance;
+}
+
 int main()
 {
+    VkInstance instance = createInstance();
+
+#ifndef NDEBUG
+    VkDebugUtilsMessengerEXT messenger = createDebugMessenger(instance);
+#endif
+
+#ifndef NDEBUG
+    destroyDebugMessenger(instance, messenger);
+#endif
+
+    vkDestroyInstance(instance, nullptr);
     return 0;
 }
